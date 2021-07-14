f6838b224952810b6b8f0d3af73873d2e27ce59e
sohel123 <sohel.abbasi@yash.com>
2021-07-14 21:20:26 +0530

    latest code for jenkins on 14072021

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -2,8 +2,14 @@ package com.collection;
 
 public class Main {
     public static void main(String[] args){
-        for(int i=0; i<5; i++){
+        for(int i=0; i<=5; i++){
             System.out.println(i);
         }
     }
+
+
+
+
+
+
 }

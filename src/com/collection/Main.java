97627bb323d4b46158da9dfcf53614629f12433e
sohel123 <sohel.abbasi@yash.com>
2021-07-14 21:29:01 +0530

    new code

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -2,7 +2,7 @@ package com.collection;
 
 public class Main {
     public static void main(String[] args){
-        for(int i=0; i<=5; i++){
+        for(int i=0; i<=4; i++){
             System.out.println(i);
         }
     }

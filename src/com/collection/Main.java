e94971c22b563947c9d6f8a384e5b225565fdb42
Syntax <instructor@syntaxtechs.com>
2021-08-10 06:23:23 +0530

    Update Main.java

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -2,9 +2,7 @@ package com.collection;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 7; i++) {
-
-            
+        for (int i = 0; i <= 5; i++) {
             System.out.println(i);
         }
     }

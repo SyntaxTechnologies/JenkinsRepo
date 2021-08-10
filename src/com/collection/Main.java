db0ac4505e53846918056fb9014d6e9114d4f910
sohel123 <sohel.abbasi@yash.com>
2021-08-10 06:13:59 +0530

    lc

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,6 +3,8 @@ package com.collection;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i <= 7; i++) {
+
+            
             System.out.println(i);
         }
     }

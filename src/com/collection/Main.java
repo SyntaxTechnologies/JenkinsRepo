a217f3d315b55f675d566a9d169a0e47aa454fe3
sohel123 <sohel.abbasi@yash.com>
2021-08-10 06:13:16 +0530

    lc08102021

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -2,10 +2,8 @@ package com.collection;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i <= 7; i++) {
             System.out.println(i);
-
-            
         }
     }
 }

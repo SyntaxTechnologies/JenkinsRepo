6afe3b8fc55674c4bb1a14f5f896bf5914e796b1
sohel123 <sohel.abbasi@yash.com>
2021-08-10 06:56:01 +0530

    gf fg

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,8 +3,6 @@ package com.collection;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i <= 5; i++) {
-
-            
             System.out.println(i);
         }
     }
